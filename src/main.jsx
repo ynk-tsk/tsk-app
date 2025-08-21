bb69b58ad1d1b6e44adc226fdef741a081abbe3b
ynk-tsk <ynk@thesatoshikoalas.com>
2025-08-22 00:32:31 +0200

    Correction du fichier de démarrage main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// This is the root of your application.
+// It finds the 'root' div in your index.html and injects your App component into it.
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
