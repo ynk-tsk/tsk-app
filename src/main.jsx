e6c93115e15ca264956a7e13e92605d51d1a5125
ynk-tsk <ynk@thesatoshikoalas.com>
2025-08-22 10:32:00 +0200

    Correction finale de l'importation CSS et des configurations

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+// CETTE LIGNE EST LA PLUS IMPORTANTE
+import './index.css' 
 
-// This is the root of your application.
-// It finds the 'root' div in your index.html and injects your App component into it.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+)
